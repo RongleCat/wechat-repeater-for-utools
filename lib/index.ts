0cf26d939226ff5c0ed654cb94691d21253e68df
ronglecat <ronglecat@163.com>
2023-02-10 15:58:28 +0800

    feat: 增加对 windows 的支持

--- lib/index.ts
+++ lib/index.ts
@@ -4,9 +4,23 @@ window.exports = {
     args: {
       enter: async () => {
         utools.hideMainWindow();
-        utools.simulateKeyboardTap('tab');
-        utools.simulateKeyboardTap('v', 'command');
-        utools.simulateKeyboardTap('enter');
+        const isWindows = utools.isWindows();
+        const isMac = utools.isMacOs();
+        if (!isMac && !isWindows) {
+          utools.showNotification('暂不支持此平台');
+          return;
+        }
+        if (isMac) {
+          utools.simulateKeyboardTap('tab');
+          utools.simulateKeyboardTap('v', 'command');
+          utools.simulateKeyboardTap('enter');
+        }
+        if (isWindows) {
+          utools.simulateKeyboardTap('tab');
+          utools.simulateKeyboardTap('tab');
+          utools.simulateKeyboardTap('v', 'ctrl');
+          utools.simulateKeyboardTap('enter');
+        }
       },
     },
   },
